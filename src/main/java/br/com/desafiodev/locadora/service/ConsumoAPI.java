d225b42c9fd97bdd7d74408b6ff346d3c5092bee
IsabelaMunzlinger <isabelamunzlinger@gmail.com>
2025-05-04 19:32:40 -0300

    Adicionando pacote service com classes para API

---
+++ src/main/java/br/com/desafiodev/locadora/service/ConsumoAPI.java
@@ -0,0 +1,31 @@
+package br.com.desafiodev.locadora.service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsumoAPI {
+
+    public String obterDados(String endereco, String token) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .header("Authorization", "Bearer " + token)
+                .header("accept", "application/json")
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        String json = response.body();
+        return json;
+    }
+}
