5b241610abc9fadc1552f290e1dfb8ffef7bed73
IsabelaMunzlinger <isabelamunzlinger@gmail.com>
2025-05-10 16:31:12 -0300

    Lombok adicionado

--- src/main/java/br/com/desafiodev/locadora/model/Exemplar.java
+++ src/main/java/br/com/desafiodev/locadora/model/Exemplar.java
@@ -2,7 +2,16 @@ package br.com.desafiodev.locadora.model;
 
 import jakarta.persistence.*;
 import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "exemplares")
 public class Exemplar {
@@ -13,6 +22,7 @@ public class Exemplar {
 
 
     @ManyToOne
+    @JoinColumn(name = "filme_id", nullable = false)
     private Filme filme;
 
 
@@ -20,40 +30,4 @@ public class Exemplar {
     private Date dataCadastro;
 
     private boolean ativo;
-
-
-
-    // Getters e setters
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Filme getFilme() {
-        return filme;
-    }
-
-    public void setFilme(Filme filme) {
-        this.filme = filme;
-    }
-
-    public Date getDataCadastro() {
-        return dataCadastro;
-    }
-
-    public void setDataCadastro(Date dataCadastro) {
-        this.dataCadastro = dataCadastro;
-    }
-
-    public boolean isAtivo() {
-        return ativo;
-    }
-
-    public void setAtivo(boolean ativo) {
-        this.ativo = ativo;
-    }
 }
