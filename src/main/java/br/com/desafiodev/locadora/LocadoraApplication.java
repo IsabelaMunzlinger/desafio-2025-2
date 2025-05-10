d6465d259bab1fefd59741eb208efb02a4260ea0
IsabelaMunzlinger <isabelamunzlinger@gmail.com>
2025-05-10 16:24:33 -0300

    Configurado para poder usar a apiTokenV4

--- src/main/java/br/com/desafiodev/locadora/LocadoraApplication.java
+++ src/main/java/br/com/desafiodev/locadora/LocadoraApplication.java
@@ -2,11 +2,13 @@ package br.com.desafiodev.locadora;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import io.github.cdimascio.dotenv.Dotenv;
 @SpringBootApplication
 public class LocadoraApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		System.setProperty("apiTokenV4", dotenv.get("apiTokenV4"));
 		SpringApplication.run(LocadoraApplication.class, args);
 	}
 
