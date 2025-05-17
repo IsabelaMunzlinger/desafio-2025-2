2f5c2c1dba1b361cce175084dc641cb454495847
IsabelaMunzlinger <isabelamunzlinger@gmail.com>
2025-05-17 20:34:23 -0300

    Endpoints de listar e editar foram adicionados

--- src/main/java/br/com/desafiodev/locadora/controller/FilmeController.java
+++ src/main/java/br/com/desafiodev/locadora/controller/FilmeController.java
@@ -1,8 +1,8 @@
 package br.com.desafiodev.locadora.controller;
 
-
 import br.com.desafiodev.locadora.dto.FilmeDTO;
 import br.com.desafiodev.locadora.model.Filme;
+import br.com.desafiodev.locadora.repository.FilmeRepository;
 import br.com.desafiodev.locadora.service.FilmeService;
 import br.com.desafiodev.locadora.service.TmdbService;
 import org.springframework.ui.Model;
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import java.util.Optional;
 
 
 @Controller
@@ -21,6 +22,9 @@ public class FilmeController {
 
     @Autowired
     private TmdbService tmdbService;
+
+    @Autowired
+    FilmeRepository filmeRepository;
 
     // Exibe formulário vazio
     @GetMapping("/form")
@@ -48,9 +52,46 @@ public class FilmeController {
             return "redirect:/filmes/form?salvo=true";
         } catch (IllegalArgumentException e) {
             model.addAttribute("filme", filmeDTO); // mantém os dados preenchidos
-            model.addAttribute("erro", e.getMessage());// passa mensagem de erro
-            return "form-filme"; // volta para o formulário com erro
+            model.addAttribute("erro", e.getMessage());
+            return "form-filme"; // volta para o formulário do filme
         }
+    }
+
+    // Mostra o formulário para edição do filme
+    @GetMapping("/editar/{id}")
+    public String mostrarFormularioEdicao(@PathVariable Long id, Model model) {
+        Optional<Filme> filmeOpt = filmeRepository.findById(id);
+        if (filmeOpt.isPresent()) {
+            model.addAttribute("filme", filmeOpt.get());
+            return "editar-filme";
+        } else {
+            return "redirect:/filmes/listar"; // redireciona para a lista de filmes
+        }
+    }
+
+
+    // Salva as edições feitas em um filme
+    @PostMapping("/editar/{id}")
+    public String salvarEdicaoFilme(@PathVariable Long id, @ModelAttribute Filme filmeEditado) {
+        Optional<Filme> filmeOriginalOpt = filmeRepository.findById(id);
+        if (filmeOriginalOpt.isPresent()) {
+            Filme filmeOriginal = filmeOriginalOpt.get();
+
+            // Só edita os campos que o usuário pode alterar
+            filmeOriginal.setExemplaresDisponiveis(filmeEditado.getExemplaresDisponiveis());   // Exemplares disponíveis
+            filmeOriginal.setAtivo(filmeEditado.isAtivo()); // Se o filme está ativo/inativo
+
+            filmeRepository.save(filmeOriginal);
+        }
+
+        return "redirect:/filmes/listar";
+    }
+
+    // Lista todos os filmes
+    @GetMapping("/listar")
+    public String listarFilmes(Model model) {
+        model.addAttribute("filmes", filmeRepository.findAll());
+        return "lista-filmes";
     }
 
 }
