087005ec6951b2d6c55fcd8ecf66f97affe7133f
IsabelaMunzlinger <isabelamunzlinger@gmail.com>
2025-05-18 16:18:11 -0300

    Adicionando busca por filmes ativos

--- src/main/java/br/com/desafiodev/locadora/repository/FilmeRepository.java
+++ src/main/java/br/com/desafiodev/locadora/repository/FilmeRepository.java
@@ -4,12 +4,18 @@ import br.com.desafiodev.locadora.model.Filme;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
+import java.util.List;
 
 @Repository
 public interface FilmeRepository extends JpaRepository<Filme, Long> {
 
-    // Não permite salvar dois filmes iguais, mesmo título
+    // Verifica se já existe um filme com o mesmo título (ignora maiúsculas/minúsculas)
     @Query("SELECT CASE WHEN COUNT(f) > 0 THEN true ELSE false END FROM Filme f WHERE LOWER(f.titulo) = LOWER(:titulo)")
     boolean verificaTituloIgnora(String titulo);
+
+    // Busca filmes ativos com pelo menos 1 exemplar disponível
+    List<Filme> findByAtivoTrueAndExemplaresDisponiveisGreaterThan(int i);
+
+    //Busca filmes ativos
+    List<Filme> findByAtivoTrue();
 }
