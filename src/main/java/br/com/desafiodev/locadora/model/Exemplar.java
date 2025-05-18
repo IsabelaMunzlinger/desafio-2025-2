bccbb97e6d33538f4ccd037bb699992aab0d5238
IsabelaMunzlinger <isabelamunzlinger@gmail.com>
2025-05-18 15:25:01 -0300

    Adicionando comentários

--- src/main/java/br/com/desafiodev/locadora/model/Exemplar.java
+++ src/main/java/br/com/desafiodev/locadora/model/Exemplar.java
@@ -1,12 +1,14 @@
 package br.com.desafiodev.locadora.model;
 
 import jakarta.persistence.*;
+
 import java.util.Date;
+import java.util.List;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
-
 
 @Getter
 @Setter
@@ -18,16 +20,22 @@ public class Exemplar {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
-
+    // Cada exemplar está associado a um único filme
     @ManyToOne
-    @JoinColumn(name = "filme_id", nullable = false)
+    @JoinColumn(name = "filme_id", nullable = true)
     private Filme filme;
 
-
+    // Data em que o exemplar foi cadastrado
     @Temporal(TemporalType.DATE)
     private Date dataCadastro;
 
+    // Indica se o exemplar está ativo
     private boolean ativo;
+
+    // Lista de locações que envolveram este exemplar
+    @OneToMany(mappedBy = "exemplar", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<LocacaoExemplar> locacoesExemplares;
+
 }
