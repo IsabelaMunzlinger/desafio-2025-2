732bffeb5dcd5e5a295bd6d4ecdf08fcf8487695
IsabelaMunzlinger <isabelamunzlinger@gmail.com>
2025-05-18 15:19:40 -0300

    Adicionando funções para formatar as datas

--- src/main/java/br/com/desafiodev/locadora/model/Locacao.java
+++ src/main/java/br/com/desafiodev/locadora/model/Locacao.java
@@ -1,110 +1,61 @@
 package br.com.desafiodev.locadora.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.List;
 
 @Entity
-@Table(name = "locacoes")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Locacao {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
-
-    @ManyToMany
-    private List<Exemplar> exemplares;
+    @Transient
+    private List<Long> exemplarIds;
 
     private String nome;
     private String cpf;
     private String email;
     private String telefone;
 
+    @NotNull
+    private LocalDate dataLocacao;
 
-    @Temporal(TemporalType.DATE)
-    private Date dataLocacao;
-
-    @Temporal(TemporalType.DATE)
-    private Date dataDevolucao;
+    @NotNull
+    private LocalDate dataDevolucao;
 
     @Temporal(TemporalType.DATE)
     private Date dataDevolvido;
 
     @Lob
-    @Column(columnDefinition = "TEXT")
+    @Column(columnDefinition = "TEXT") //TEXT para ter espaço para armazenar o valor
     private String qrCode;
 
-    public Integer getId() {
-        return id;
+    @OneToMany(mappedBy = "locacao", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<LocacaoExemplar> locacoesExemplares;
+
+    // Para formatar as datas de locação e devolução
+    public String getDataLocacaoFormatada() {
+        if (dataLocacao == null) return "";
+        return dataLocacao.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public String getDataDevolucaoFormatada() {
+        if (dataDevolucao == null) return "";
+        return dataDevolucao.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
     }
 
-    public List<Exemplar> getExemplares() {
-        return exemplares;
-    }
-
-    public void setExemplares(List<Exemplar> exemplares) {
-        this.exemplares = exemplares;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public Date getDataLocacao() {
-        return dataLocacao;
-    }
-
-    public void setDataLocacao(Date dataLocacao) {
-        this.dataLocacao = dataLocacao;
-    }
-
-    public Date getDataDevolucao() {
-        return dataDevolucao;
-    }
-
-    public void setDataDevolucao(Date dataDevolucao) {
-        this.dataDevolucao = dataDevolucao;
-    }
-
-    public Date getDataDevolvido() {
-        return dataDevolvido;
-    }
-
-    public void setDataDevolvido(Date dataDevolvido) {
-        this.dataDevolvido = dataDevolvido;
-    }
 }
